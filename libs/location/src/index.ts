56e2999f9c7e60d5bea96d4b8ba34c620d4c852a
Dan Jones <daniel.jones@nimbleapproach.com>
2023-05-15 14:59:11 +0100

    initial background commit

---
+++ libs/location/src/index.ts
@@ -0,0 +1,37 @@
+import {
+  LocationObject,
+  requestBackgroundPermissionsAsync,
+  requestForegroundPermissionsAsync,
+  startLocationUpdatesAsync,
+} from 'expo-location';
+import { defineTask } from 'expo-task-manager';
+import { sendLocationPing } from '@nx-expo/appwrite';
+
+const LOCATION_TASK_NAME = 'background-location-task';
+
+export const requestPermissions = async () => {
+  const { status: foregroundStatus } =
+    await requestForegroundPermissionsAsync();
+
+  console.log(startLocationUpdatesAsync);
+
+  if (foregroundStatus === 'granted') {
+    const { status: backgroundStatus } =
+      await requestBackgroundPermissionsAsync();
+    if (backgroundStatus === 'granted') {
+      await startLocationUpdatesAsync(LOCATION_TASK_NAME);
+    }
+  }
+};
+
+defineTask<Array<LocationObject>>(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+  if (data) {
+    const { locations } = data;
+    console.log(locations);
+    sendLocationPing(locations[0]);
+  }
+});
