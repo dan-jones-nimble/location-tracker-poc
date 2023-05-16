e1e00f862d19b750c1e16530dd0325011e5761c7
Dan Jones <daniel.jones@nimbleapproach.com>
2023-05-16 17:05:15 +0100

    rejig login page to libs, add loggedInContext to be shared across apps and web

--- apps/door-to-door-app/app/index.tsx
+++ apps/door-to-door-app/app/index.tsx
@@ -1,66 +1,37 @@
 // Imports
-import { useEffect, useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { SplashScreen } from 'expo-router';
 
 // Components
 import { TrackerPrompt } from '../components';
+import { LoginScreen } from '@nx-expo/components';
+
+// Contexts
+import { useLoggedIn } from '@nx-expo/context';
 
 // Appwrite
-import { getAccount, emailLogin } from '@nx-expo/appwrite';
+import { getAccount } from '@nx-expo/appwrite';
 
 export default function HomeScreen() {
   const [loading, setLoading] = useState<boolean>(true);
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [credentials, setCredentials] = useState<{
-    email: string;
-    password: string;
-  }>({ email: '', password: '' });
+  const { isLoggedIn, logIn } = useLoggedIn();
 
-  const handleInput = (name: string, value: string) => {
-    setCredentials((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const getUser = async () => {
+  const getUser = useCallback(async () => {
     const account = await getAccount();
-    if (account) setIsLoggedIn(true);
+    if (account) logIn();
     setLoading(false);
-  };
-
-  const login = async () => {
-    const login = await emailLogin({
-      email: credentials.email,
-      password: credentials.password,
-    });
-    if (login?.$id) setIsLoggedIn(true);
-  };
+  }, [logIn]);
 
   useEffect(() => {
     getUser();
-  }, []);
+  }, [getUser]);
 
   return (
     <View style={styles.container}>
       {loading && <SplashScreen />}
       {!loading && isLoggedIn && <TrackerPrompt />}
-      {!loading && !isLoggedIn && (
-        <>
-          <TextInput
-            style={styles.textInput}
-            onChange={(e) => handleInput('email', e.nativeEvent.text)}
-            autoFocus
-          />
-          <TextInput
-            style={styles.textInput}
-            onChange={(e) => handleInput('password', e.nativeEvent.text)}
-            aria-hidden
-          />
-          <Button title="Submit" onPress={login} />
-        </>
-      )}
+      {!loading && !isLoggedIn && <LoginScreen />}
     </View>
   );
 }
@@ -70,15 +41,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    gap: 4,
-  },
-  textInput: {
-    display: 'flex',
-    fontSize: 16,
-    minWidth: 300,
-    borderColor: '#000',
-    borderWidth: 3,
-    padding: 4,
-    borderRadius: 5,
-  },
+    gap: 4
+  }
 });
