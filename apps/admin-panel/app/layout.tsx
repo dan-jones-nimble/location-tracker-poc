a40b3c51320e3975023534e6dcbb4b086e61842f
Jordan Chesham <110815535+jordan-nimble@users.noreply.github.com>
2023-05-16 11:11:39 +0100

    Remove node appwrite

--- apps/admin-panel/app/layout.tsx
+++ apps/admin-panel/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import Head from 'next/head';
 
 import { getAccount } from '@nx-expo/appwrite';
 
@@ -27,9 +28,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      {/* <Head>
+      <Head>
         <title>Welcome to admin-panel!</title>
-      </Head> */}
+      </Head>
       <body>{!loading && children}</body>
     </html>
   );
