f45e86aeda10d197972c84f99ed3f65ac2f68d30
Dan Jones <daniel.jones@nimbleapproach.com>
2023-06-07 10:27:28 +0100

    rename

--- apps/delivery-app/components/IdListTable.tsx
+++ apps/delivery-app/components/IdListTable.tsx
@@ -2,7 +2,7 @@ import { MinimalLocationObject, RouteObject } from '@nx-expo/appwrite';
 import { openURL } from 'expo-linking';
 import { View, Text, StyleSheet, Pressable } from 'react-native';
 
-const buildWaypointsUrl = (waypoints: Array<MinimalLocationObject>) => {
+const buildGMapsUrl = (waypoints: Array<MinimalLocationObject>) => {
   const firstWaypoint = waypoints.shift();
   const lastWaypoint = waypoints.pop();
   const remainingWaypoints = waypoints.map(
@@ -23,17 +23,15 @@ const buildWaypointsUrl = (waypoints: Array<MinimalLocationObject>) => {
 
 export const IdListTable = (props: { routes: Array<RouteObject> }) => {
   const handleButtonPress = (route: RouteObject) => {
-    const gMapsUrl = buildWaypointsUrl(route.location_pings);
+    const gMapsUrl = buildGMapsUrl(route.location_pings);
     openURL(gMapsUrl).catch((error) => console.error(error));
   };
 
   return (
     <View style={styles.tableView}>
       {props.routes.map((route) => (
-        <Pressable onPress={() => handleButtonPress(route)}>
-          <Text style={styles.text} key={route.$id}>
-            {route.$id}
-          </Text>
+        <Pressable key={route.$id} onPress={() => handleButtonPress(route)}>
+          <Text style={styles.text}>{route.$id}</Text>
         </Pressable>
       ))}
     </View>
